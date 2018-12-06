bf87c71a6340023d980876237ecd38a770d3fb74
Jonathan Twite <13119611+jonathantwite@users.noreply.github.com>
2018-12-06 12:16:04 +0000

    Requirejs setup and page-title component

---
+++ RequireAKnockout/scripts/app/app.js
@@ -0,0 +1,22 @@
+﻿requirejs.config({
+    baseUrl: 'scripts/app',
+    paths: {
+        "knockout": "../vendor/knockout-3.3.0",
+        "text": "../vendor/text",
+    }
+});
+
+require(['knockout'], function (ko) {
+    ko.components.register('page-title', { require: 'components/page-title' });
+
+    var ViewModel = function () {
+        var self = this;
+
+        self.pageTitle = ko.observable('Require a knockout');
+    }
+
+    var vm = new ViewModel();
+    ko.applyBindings(vm);
+    
+    return vm;
+});
